13a32562bf7f00dcf5c4e0838e4d249f898b82ec
Victor Lia Fook <victorliafook@gmail.com>
2017-04-23 13:14:08 +0000

    heroku setup

--- lib/conn.php
+++ lib/conn.php
@@ -1,11 +1,12 @@
 <?php
 
 function connect_db() {
-	$server = 'localhost'; 
-	$user = $_ENV["CL_MYSQL_USER"];
-	$pass = $_ENV["CL_MYSQL_PASS"];
-	$database = 'cl_client_lookup';
-	$connection = new mysqli($server, $user, $pass, $database);
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$database = substr($url["path"], 1);
 	if ($connection->connect_error) {
 	    die("Connection failed: " . $connection->connect_error);
 	} 
