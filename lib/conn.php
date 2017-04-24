cde564090e850e5116681e330b1ef7ec21d3261f
Victor Lia Fook <victorliafook@gmail.com>
2017-04-24 01:21:34 +0000

    prepare to  branch for heroku

--- lib/conn.php
+++ lib/conn.php
@@ -1,6 +1,21 @@
 <?php
 
+function connect_db() {
+	
+	$server = 'localhost'; 
+	$user = $_ENV["CL_MYSQL_USER"];
+	$pass = $_ENV["CL_MYSQL_PASS"];
+	$database = 'cl_client_lookup';
+	$connection = new mysqli($server, $user, $pass, $database);
+	if ($connection->connect_error) {
+	    die("Connection failed: " . $connection->connect_error);
+	} 
+    return $connection;
+}
 
+/**
+*	For Heroku App
+*  
 function connect_db() {
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
@@ -15,3 +30,4 @@ function connect_db() {
 	} 
     return $connection;
 }
+*/
